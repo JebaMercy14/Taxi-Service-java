57e03140f7c4ba18654a629b78f50ba3b0c278f4
Kothai_S <102270819+JebaMercy14@users.noreply.github.com>
2024-05-22 16:23:07 +0530

    Create InvalidRideException.java

---
+++ InvalidRideException.java
@@ -0,0 +1,10 @@
+/**
+ * Throw this exception class in TaxiService.validateRide(Ride) method 
+ * when the ride is not valid. There is nothing required to do in this class.
+ */
+public class InvalidRideException extends Exception{
+     public InvalidRideException(String message) { 
+         super(message); 
+     } 
+}
+
