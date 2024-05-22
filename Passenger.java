829b8046db40f36a77fa8558bb56232a460ad11b
Kothai_S <102270819+JebaMercy14@users.noreply.github.com>
2024-05-22 16:23:52 +0530

    Create Passenger.java

---
+++ Passenger.java
@@ -0,0 +1,23 @@
+public class Passenger extends User {
+	
+	// BUSINESS, PERSONAL, etc.
+	private String accountType;
+
+	public Passenger() {
+		super();
+	}
+
+	public Passenger(String accountType) {
+		super();
+		this.accountType = accountType;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+}
